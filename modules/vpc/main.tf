bbd53894799afb62ed62a49a43676eefb20fb13c
rushidholecom <dholerushi.com@gmail.com>
2026-03-05 15:52:10 +0530

    fix

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "public_subnet" {
     name = "${var.project}-public-subnet"
   }
   cidr_block = var.public_subnet_cidr
-  vpc_id = aws_vpc.project_vpc.id
+  vpc_id = "${aws_vpc.project_vpc.id}"
   map_public_ip_on_launch = true
   availability_zone = var.public_avaibilityzone
 }
@@ -21,7 +21,7 @@ resource "aws_subnet" "private_subnet" {
     name = "${var.project}-private-subnet"
   }
   cidr_block = var.private_subnet_cidr
-  vpc_id = aws_vpc.project_vpc.id
+  vpc_id = "${aws_vpc.project_vpc.id}"
   map_public_ip_on_launch = false
   availability_zone = var.private_avaibilityzone
 }
