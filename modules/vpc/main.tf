2daf3196d638b3eed3794451f50930160414cf60
rushidholecom <dholerushi.com@gmail.com>
2026-03-05 15:30:26 +0530

    create subents

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,8 +1,27 @@
 resource "aws_vpc" "project_vpc" {
   tags = {
-    name = "project_vpc"
+    name = "project-vpc"
   }
   cidr_block = var.cidr
   instance_tenancy = "default"
-  
+}
+
+resource "aws_subnet" "public_subnet" {
+  tags = {
+    name = "${var.project}-public-subnet"
+  }
+  cidr_block = var.public_subnet_cidr
+  vpc_id = aws_vpc.project_vpc.id
+  map_customer_owned_ip_on_launch = "true"
+  availability_zone = var.public_avaibilityzone
+}
+
+resource "aws_subnet" "private_subnet" {
+  tags = {
+    name = "${var.project}-private-subnet"
+  }
+  cidr_block = var.public_subnet_cidr
+  vpc_id = aws_vpc.project_vpc.id
+  map_customer_owned_ip_on_launch = "false"
+  availability_zone = var.private_avaibilityzone
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,21 @@
+variable "project" {
+  default = "three-tier"
+}
+
 variable "cidr" {}
+
+variable "public_subnet_cidr" {
+  default = "171.10.0.0/22"
+}
+
+variable "private_subnet_cidr" {
+  default = "171.10.1.0/22"
+}
+
+variable "public_avaibilityzone" {
+  default = "ap-southeast-1a"
+}
+
+variable "private_avaibilityzone" {
+  default = "ap-southeast-1b"
+}
